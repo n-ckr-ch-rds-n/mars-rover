af9d98d5c0d5e5dac5cd7edb630169b6c06e97a4
Nick <nick.richardson@rezonence.com>
2020-03-11 23:05:10 +0000

    Create orientation service class and test file

---
+++ src/orientation-service/orientation.service.spec.ts
@@ -0,0 +1,9 @@
+import {OrientationService} from "./orientation.service";
+
+describe("Orientation service", () => {
+    let orientationService: OrientationService;
+
+    beforeEach(() => {
+        orientationService = new OrientationService()
+    })
+});
