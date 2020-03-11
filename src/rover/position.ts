6fb6bf1b4423dd4be7e01358ea5656555cebd928
Nick <nick.richardson@rezonence.com>
2020-03-11 20:58:06 +0000

    Create position interface

---
+++ src/rover/position.ts
@@ -0,0 +1,6 @@
+import {RoverCoordinates} from "./rover.coordinates";
+import {Orientation} from "./orientation";
+
+export interface Position extends RoverCoordinates {
+    orientation: Orientation;
+}
