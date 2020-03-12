eeb78b1e718cd6bbf4dfa84b748c1ed9241051f4
Nick <nick.richardson@rezonence.com>
2020-03-12 00:13:00 +0000

    Create coordinates service and test file

---
+++ src/coordinates-service/coordinates.service.spec.ts
@@ -0,0 +1,9 @@
+import {CoordinatesService} from "./coordinates.service";
+
+describe("Coordinates service", () =>{
+    let coordinatesService: CoordinatesService;
+
+    beforeEach(() => {
+        coordinatesService = new CoordinatesService();
+    });
+});
