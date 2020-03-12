fb3339de6acffea4414321e750a18430a19922e4
Nick <nick.richardson@rezonence.com>
2020-03-12 00:52:53 +0000

    Pass coordinates service into rover constructor

--- src/rover/rover.ts
+++ src/rover/rover.ts
@@ -1,33 +1,26 @@
 import {Position} from "../position";
-import {RoverCoordinates} from "./rover.coordinates";
-import {Orientation} from "../orientation";
 import {Rotation} from "../rotation";
 import {OrientationService} from "../orientation-service/orientation.service";
+import {CoordinatesService} from "../coordinates-service/coordinates.service";
 
 export class Rover {
 
     position: Position;
 
-    constructor(private orientationService: OrientationService, initialPosition: Position) {
+    constructor(private orientationService: OrientationService,
+                private coordinatesService: CoordinatesService,
+                initialPosition: Position) {
         this.position = {...initialPosition};
     }
 
     move() {
-        this.position.coordinates[this.affectedAxis()] += this.direction();
+        this.position.coordinates = this.coordinatesService.refreshCoordinates(this.position);
     }
 
     turn(rotation: Rotation) {
         this.position.orientation = this.orientationService.reorient({
             rotation, currentOrientation: this.position.orientation
         });
-    }
-
-    private affectedAxis(): keyof RoverCoordinates {
-        return [Orientation.North, Orientation.South].includes(this.position.orientation) ? "y" : "x";
-    }
-
-    private direction(): 1 | -1 {
-        return [Orientation.North, Orientation.East].includes(this.position.orientation) ? 1 : -1;
     }
 
 }
