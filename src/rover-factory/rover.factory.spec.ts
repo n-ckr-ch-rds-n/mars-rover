b477757f40a925c9490cc3ce35976597461da2cc
nick_richardson <nick.richardson@rezonence.com>
2020-03-15 18:53:26 +0000

    Create unit test and stub class for Rover factory

---
+++ src/rover-factory/rover.factory.spec.ts
@@ -0,0 +1,12 @@
+import {Rover} from "../rover/rover";
+import {RoverFactory} from "./rover.factory";
+
+describe("Rover factory", () => {
+
+    let factory: RoverFactory;
+
+    beforeEach(() => {
+        factory = new RoverFactory();
+    })
+
+});
