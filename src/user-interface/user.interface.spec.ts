04bf9eafccc137961b0510bf855e5235a047bc07
nick_richardson <nick.richardson@rezonence.com>
2020-03-15 14:03:43 +0000

    Test that a readline interface is created on instantiating ui interface class

--- src/user-interface/user.interface.spec.ts
+++ src/user-interface/user.interface.spec.ts
@@ -1,9 +1,18 @@
 import {UserInterface} from "./user.interface";
+import rl, {Interface, ReadLineOptions} from "readline";
+import {expect} from "chai";
 
 describe("User interface", () => {
     let ui: UserInterface;
+    let mockInterface: Interface;
 
     beforeEach(() => {
+        mockInterface = {} as Interface;
+        rl.createInterface = () => mockInterface;
         ui = new UserInterface();
+    });
+
+    it("Creates an interface on instantiation", () => {
+        expect(ui.interface).to.deep.equal(mockInterface, "Interface should have been created");
     })
 });
--- src/user-interface/user.interface.ts
+++ src/user-interface/user.interface.ts
@@ -1,3 +1,13 @@
+import rl, {Interface} from "readline";
+
 export class UserInterface {
+    interface: Interface;
+
+    constructor() {
+        this.interface = rl.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        })
+    }
 
 }
