d99195f952849a657732fb22a477a82d78eeb3a3
nick_richardson <nick.richardson@rezonence.com>
2020-03-15 20:14:40 +0000

    Resolve request text by input type

--- src/user-interface/user.interface.ts
+++ src/user-interface/user.interface.ts
@@ -2,27 +2,27 @@ import {RoverInterface} from "../interface-factory/rover.interface";
 import {InputValidator} from "../input-validator/input-validator";
 import {InputType} from "../input-validator/input.type";
 import {ValidatorResponse} from "../input-validator/validator.response";
+import {RoverFactory} from "../rover-factory/rover.factory";
 
 export class UserInterface {
 
-    output: Record<string, string> = {
-        instructionsRequest: "Please input Rover navigation instructions. Format: 'LRMRLMM'. Permitted characters: L, R, M\n",
-        initialPositionRequest: "Please input"
+    output: Record<InputType, string> = {
+        [InputType.Instructions]: "Please input Rover navigation instructions. Format: 'LRMRLMM'. Permitted characters: L, R, M\n",
+        [InputType.InitialCoordinates]: "Please input"
     };
 
     constructor(private roverInterface: RoverInterface,
-                private validator: InputValidator) {
+                private validator: InputValidator,
+                private roverFactory: RoverFactory) {
     }
 
     async start() {
-        const roverInstructions = await this.requestInput({
-            requestText: this.output.instructionsRequest, type: InputType.Instructions
-        });
+        const roverInstructions = await this.requestInput(InputType.Instructions);
     }
 
-    async requestInput(request: {requestText: string, type: InputType}): Promise<ValidatorResponse> {
-        const input = await this.roverInterface.questionAsync(request.requestText);
+    async requestInput(type: InputType): Promise<ValidatorResponse> {
+        const input = await this.roverInterface.questionAsync(this.output[type]);
         this.roverInterface.close();
-        return this.validator.validate({input, type: request.type});
+        return this.validator.validate({input, type});
     }
 }
