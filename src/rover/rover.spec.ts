0e3cfbf9dacee36a4dc064a20f39d1aebef46dd9
nick_richardson <nick.richardson@rezonence.com>
2020-03-15 13:39:12 +0000

    Make naming in rover test more explicit

--- src/rover/rover.spec.ts
+++ src/rover/rover.spec.ts
@@ -14,57 +14,57 @@ describe("Rover", () => {
     let mockOrientationService: OrientationService;
     let mockCoordinatesService: CoordinatesService;
     let mockInitialPosition: Position;
-    let mockCoordinates: RoverCoordinates;
+    let mockUpdatedCoordinates: RoverCoordinates;
     let mockX: number;
     let mockY: number;
-    let mockOrientation: Orientation;
+    let mockUpdatedOrientation: Orientation;
     let newPosition: Position;
 
     beforeEach(() => {
         mockX = 1;
         mockY = 3;
         mockInitialPosition = {coordinates: {x: mockX, y: mockY}, orientation: Orientation.North};
-        mockOrientation = Orientation.West;
+        mockUpdatedOrientation = Orientation.West;
         mockOrientationService = {
-          reorient: (request: ReorientationRequest) => mockOrientation
+          reorient: (request: ReorientationRequest) => mockUpdatedOrientation
         } as OrientationService;
         mockCoordinatesService = {
-            refreshCoordinates: (currentPosition: Position) => mockCoordinates
+            refreshCoordinates: (currentPosition: Position) => mockUpdatedCoordinates
         } as CoordinatesService;
         rover = new Rover(mockOrientationService, mockCoordinatesService, mockInitialPosition);
     });
 
     it("Moves", () => {
         for (const coords of [{x: 3, y: 1}, {x: 2, y: 4}, {x: -1, y: 500}]) {
-            mockCoordinates = coords;
+            mockUpdatedCoordinates = coords;
             rover.move();
-            expect(rover.position.coordinates).to.deep.equal(mockCoordinates, "Rover has not moved");
+            expect(rover.position.coordinates).to.deep.equal(mockUpdatedCoordinates, "Rover has not moved");
         }
     });
 
     it("Rotates", () => {
         for (const rotation of Object.values(Rotation)) {
             rover.turn(rotation);
-            expect(rover.position.orientation).to.equal(mockOrientation, "Rover has not been reoriented")
+            expect(rover.position.orientation).to.equal(mockUpdatedOrientation, "Rover has not been reoriented")
         }
     });
 
     it("Understands orientation instructions", () => {
         newPosition = rover.explore([Rotation.Left, Rotation.Right]);
         expect(newPosition.coordinates).to.deep.equal(mockInitialPosition.coordinates, "Rover should not have moved");
-        expect(newPosition.orientation).to.equal(mockOrientation, "Rover should have reoriented");
+        expect(newPosition.orientation).to.equal(mockUpdatedOrientation, "Rover should have reoriented");
     });
 
     it("Understands movement instructions", () => {
        newPosition = rover.explore([Movement.Forward]);
        expect(newPosition.orientation).to.equal(mockInitialPosition.orientation, "Rover should not have reoriented")
-       expect(newPosition.coordinates).to.deep.equal(mockCoordinates);
+       expect(newPosition.coordinates).to.deep.equal(mockUpdatedCoordinates);
     });
 
     it("Understands a mixed list of instructions", () => {
         newPosition = rover.explore([Rotation.Right, Movement.Forward, Rotation.Left]);
-        expect(newPosition.orientation).to.equal(mockOrientation, "Rover should have reoriented");
-        expect(newPosition.coordinates).to.deep.equal(mockCoordinates);
+        expect(newPosition.orientation).to.equal(mockUpdatedOrientation, "Rover should have reoriented");
+        expect(newPosition.coordinates).to.deep.equal(mockUpdatedCoordinates);
     });
 
 });
