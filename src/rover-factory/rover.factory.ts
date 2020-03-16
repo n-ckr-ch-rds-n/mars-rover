1297899c40f8dec80880d5f5e81e9c8859ea4873
nick_richardson <nick.richardson@rezonence.com>
2020-03-16 11:35:12 +0000

    Add plateau to rover factory creation request

--- src/rover-factory/rover.factory.ts
+++ src/rover-factory/rover.factory.ts
@@ -2,13 +2,14 @@ import {Rover} from "../rover/rover";
 import {OrientationService} from "../orientation-service/orientation.service";
 import {Position} from "../rover/position";
 import {CoordinatesService} from "../coordinates-service/coordinates.service";
+import {Plateau} from "../plateau/plateau";
 
 export class RoverFactory {
     constructor() {}
 
-    create(initialPosition: Position): Rover {
+    create(request: {initialPosition: Position, plateau: Plateau}): Rover {
         const orientationService = new OrientationService();
-        const coordinatesService = new CoordinatesService();
-        return new Rover(orientationService, coordinatesService, initialPosition);
+        const coordinatesService = new CoordinatesService(request.plateau);
+        return new Rover(orientationService, coordinatesService, request.initialPosition);
     }
 }
