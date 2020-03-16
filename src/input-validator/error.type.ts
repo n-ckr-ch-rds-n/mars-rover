953ac2addbb52cb142e4836f38cdfa7f37acce52
nick_richardson <nick.richardson@rezonence.com>
2020-03-16 12:51:29 +0000

    Create an error type enum for fetching error messages

---
+++ src/input-validator/error.type.ts
@@ -0,0 +1,3 @@
+export enum ErrorType {
+    WrongLength = "Wrong length"
+}
--- src/input-validator/input-validator.ts
+++ src/input-validator/input-validator.ts
@@ -1,10 +1,11 @@
 import {ValidatorResponse} from "./validator.response";
 import {ValidationRequest} from "./validation.request";
+import {ErrorType} from "./error.type";
 
 export class InputValidator {
 
-    errors: Record<string, string> = {
-        wrongNumberOfCharacters: "Wrong number of characters in input"
+    errors: Record<ErrorType, string> = {
+        [ErrorType.WrongLength]: "Wrong number of characters in input"
     };
 
     constructor() {}
@@ -15,7 +16,7 @@ export class InputValidator {
 
     validateCoordinates(input: string[]): ValidatorResponse {
         if (input.length !== 2) {
-            return this.toError(this.errors.wrongNumberOfCharacters);
+            return this.toError(this.errors[ErrorType.WrongLength]);
         }
     }
 
