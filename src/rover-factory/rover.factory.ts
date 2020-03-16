8cbaa3fa55b7af200d6605ec53adf53c97dc2f36
nick_richardson <nick.richardson@rezonence.com>
2020-03-16 15:47:55 +0000

    Throw error if Rover initialised out of bounds

--- src/rover-factory/rover.factory.ts
+++ src/rover-factory/rover.factory.ts
@@ -3,6 +3,7 @@ import {OrientationService} from "../orientation-service/orientation.service";
 import {Position} from "../rover/position";
 import {CoordinatesService} from "../coordinates-service/coordinates.service";
 import {Plateau} from "../plateau/plateau";
+import {ErrorType} from "../input-validator/error.type";
 
 export class RoverFactory {
     constructor() {}
@@ -10,6 +11,10 @@ export class RoverFactory {
     create(request: {initialPosition: Position, plateau: Plateau}): Rover {
         const orientationService = new OrientationService();
         const coordinatesService = new CoordinatesService(request.plateau);
-        return new Rover(orientationService, coordinatesService, request.initialPosition);
+        if (!request.plateau.outOfBounds(request.initialPosition.coordinates)) {
+            return new Rover(orientationService, coordinatesService, request.initialPosition);
+        } else {
+            throw new Error(ErrorType.OutOfBounds);
+        }
     }
 }
