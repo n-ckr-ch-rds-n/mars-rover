7899c156a7357d1f5f295f829d933f9bb920f1f4
nick_richardson <nick.richardson@rezonence.com>
2020-03-16 10:31:43 +0000

    Pass upper right coords into plateau constructor

--- src/plateau/plateau.spec.ts
+++ src/plateau/plateau.spec.ts
@@ -3,8 +3,8 @@ import {Plateau} from "./plateau";
 describe("Plateau", () => {
     let plateau: Plateau;
 
-    beforeEach(() => {
-        plateau = new Plateau();
+    it("Can be initialised with different upper right coordinates", () => {
+        plateau = new Plateau({x: 3, y: 4});
     })
 
 });
--- src/plateau/plateau.ts
+++ src/plateau/plateau.ts
@@ -1,3 +1,13 @@
+import {Coordinates} from "../coordinates-service/coordinates";
+
 export class Plateau {
 
+    area = {
+        upperRightCoords: {},
+        bottomLeftCoords: {x: 0, y: 0}
+    };
+
+    constructor(upperRightCoords: Coordinates) {
+        this.area.upperRightCoords = upperRightCoords
+    }
 }
