54706c12dc5953322539516d76c0bf20752b61ed
nick_richardson <nick.richardson@rezonence.com>
2020-03-16 16:16:14 +0000

    Pass mock plateau to rover factory in test

--- src/rover-factory/rover.factory.spec.ts
+++ src/rover-factory/rover.factory.spec.ts
@@ -1,22 +1,27 @@
 import {RoverFactory} from "./rover.factory";
-import {Coordinates} from "../rover/rover.coordinates";
+import {Coordinates} from "../coordinates-service/coordinates";
 import {Position} from "../rover/position";
 import {Orientation} from "../orientation-service/orientation";
 import {expect} from "chai";
+import {Plateau} from "../plateau/plateau";
 
 describe("Rover factory", () => {
     let factory: RoverFactory;
     let coords: Coordinates;
     let initialPosition: Position;
+    let mockPlateau: Plateau;
 
     beforeEach(() => {
         coords = {x: 1, y: 3};
         initialPosition = {coordinates: coords, orientation: Orientation.East};
+        mockPlateau = {
+            outOfBounds: (coords: Coordinates) => false
+        } as Plateau;
         factory = new RoverFactory();
     });
 
     it("Creates Rovers", () => {
-        const rover = factory.create(initialPosition);
+        const rover = factory.create({initialPosition, plateau: mockPlateau});
         expect(rover.position).to.deep.equal(initialPosition,
             "Rover should have been created with an initial position");
         for (const service of ["orientationService", "coordinatesService"]) {
