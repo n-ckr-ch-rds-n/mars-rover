d2bee6252bf70f9d37bb10c0f3348d39a8fe98f2
nick_richardson <nick.richardson@rezonence.com>
2020-03-16 16:22:09 +0000

    Test that rover factory throws an error if initial position input is out of bounds

--- src/rover-factory/rover.factory.spec.ts
+++ src/rover-factory/rover.factory.spec.ts
@@ -4,6 +4,7 @@ import {Position} from "../rover/position";
 import {Orientation} from "../orientation-service/orientation";
 import {expect} from "chai";
 import {Plateau} from "../plateau/plateau";
+import {ErrorType} from "../input-validator/error.type";
 
 describe("Rover factory", () => {
     let factory: RoverFactory;
@@ -33,5 +34,14 @@ describe("Rover factory", () => {
             expect(Object.keys(rover)).to.include(service, "Rover not created with necessary services");
         }
     });
+
+    it("Doesn't create Rovers if the initial position input is out of bounds", () => {
+        mockPlateau.outOfBounds = (coords) => true;
+        try {
+            factory.create({initialPosition, plateau: mockPlateau});
+        } catch (error) {
+            expect(error.message).to.equal(ErrorType.OutOfBounds, "Should have thrown an out of bounds error");
+        }
+    })
 
 });
