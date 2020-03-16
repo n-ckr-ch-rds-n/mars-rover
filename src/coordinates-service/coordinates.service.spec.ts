2f216027afccd65f1310bd0b28d04840d9fb7f17
nick_richardson <nick.richardson@rezonence.com>
2020-03-16 11:50:48 +0000

    Initialise coordinates service with a mock plateau in test

--- src/coordinates-service/coordinates.service.spec.ts
+++ src/coordinates-service/coordinates.service.spec.ts
@@ -2,17 +2,20 @@ import {CoordinatesService} from "./coordinates.service";
 import {Orientation} from "../orientation-service/orientation";
 import {Position} from "../rover/position";
 import {expect} from "chai";
+import {Plateau} from "../plateau/plateau";
 
 describe("Coordinates service", () =>{
     let coordinatesService: CoordinatesService;
     let mockPosition: Position;
+    let mockPlateau: Plateau;
     let mockX: number;
     let mockY: number;
 
     beforeEach(() => {
         mockX = 1;
         mockY = 3;
-        coordinatesService = new CoordinatesService();
+        mockPlateau = {area: {upperRight: {x: 5, y: 5}, bottomLeft: {x: 0, y: 0}}};
+        coordinatesService = new CoordinatesService(mockPlateau);
     });
 
     it("Increases y axis value by a degree if oriented Northward", () => {
