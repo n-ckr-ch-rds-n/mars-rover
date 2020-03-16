91ee91b480939e061af9cd93289d27098f69a70c
nick_richardson <nick.richardson@rezonence.com>
2020-03-16 12:49:12 +0000

    Implement length check for coordinates

--- src/input-validator/input-validator.spec.ts
+++ src/input-validator/input-validator.spec.ts
@@ -10,7 +10,7 @@ describe("Input validator", () => {
     });
 
     it("Sanitises string input", () => {
-        const sanitised = validator.sanitise("M J , 3  %^&T");
+        const sanitised = validator.sanitise("M j , 3  %^&T");
         const expected = ["M", "J", "3", "T"];
         expect(sanitised).to.deep.equal(expected, "Input was not sanitised correctly");
     });
@@ -19,9 +19,10 @@ describe("Input validator", () => {
     describe("Coordinate validation", () => {
 
         it("Marks input as invalid if too long or short", () => {
-            const validated = validator.validateCoordinates(["3", "4", "M", "G"])
+            const validated = validator.validateCoordinates(["3", "4", "M", "G"]);
             expect(validated.valid).to.equal(false, "Input should have been marked invalid")
-        })
+        });
+
     })
 
 
--- src/input-validator/input-validator.ts
+++ src/input-validator/input-validator.ts
@@ -2,6 +2,11 @@ import {ValidatorResponse} from "./validator.response";
 import {ValidationRequest} from "./validation.request";
 
 export class InputValidator {
+
+    errors: Record<string, string> = {
+        wrongNumberOfCharacters: "Wrong number of characters in input"
+    };
+
     constructor() {}
 
     validate(request: ValidationRequest): ValidatorResponse{
@@ -9,10 +14,19 @@ export class InputValidator {
     }
 
     validateCoordinates(input: string[]): ValidatorResponse {
-
+        if (input.length !== 2) {
+            return this.toError(this.errors.wrongNumberOfCharacters);
+        }
     }
 
     sanitise(input: string): string[] {
-        return input.replace(/[^0-9a-z]/gi, '').split("")
+        return input
+            .replace(/[^0-9a-z]/gi, '')
+            .toUpperCase()
+            .split("")
+    }
+
+    toError(message: string): ValidatorResponse {
+        return {valid: false, error: message};
     }
 }
